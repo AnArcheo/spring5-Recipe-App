d221676d96e40b601da90df156db3e8d695295fd
Anna Łuczak <an.archeo@gmail.com>
2022-02-07 00:19:49 +0000

    Updated IndexController

--- src/main/java/com/springframework/spring5recipeapp/contollers/IndexController.java
+++ src/main/java/com/springframework/spring5recipeapp/contollers/IndexController.java
@@ -4,28 +4,29 @@ import com.springframework.spring5recipeapp.domain.Category;
 import com.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import com.springframework.spring5recipeapp.repositories.CategoryRepository;
 import com.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import com.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
-
+@Slf4j
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
-    @RequestMapping({"","/", "/index"})
-    public String getIndexPage(){
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-        System.out.println("Cat Id is: " + categoryOptional.get().getId());
-        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(Model model) {
+        log.debug("Getting Index page");
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 }
