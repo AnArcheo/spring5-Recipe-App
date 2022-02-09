4a3edf47c72db9473f93ec9914ff6401d8cdaad7
Anna Łuczak <an.archeo@gmail.com>
2022-02-09 22:42:32 +0000

    added lombok to Index Controller

--- src/main/java/com/springframework/spring5recipeapp/contollers/IndexController.java
+++ src/main/java/com/springframework/spring5recipeapp/contollers/IndexController.java
@@ -1,12 +1,13 @@
 package com.springframework.spring5recipeapp.contollers;
 
 import com.springframework.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -18,7 +19,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
-
+        log.debug("Getting Index Page");
         model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
