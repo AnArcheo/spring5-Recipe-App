53487f8f6f4256ee8d5fa47cd2200052695132b0
Anna Łuczak <an.archeo@gmail.com>
2022-02-09 22:45:13 +0000

    added lombok and refactored Ingredient Entity

--- src/main/java/com/springframework/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/com/springframework/spring5recipeapp/domain/Ingredient.java
@@ -1,8 +1,11 @@
 package com.springframework.spring5recipeapp.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
 @Entity
 public class Ingredient {
     @Id
@@ -34,43 +37,4 @@ public class Ingredient {
         this.recipe = recipe;
     }
 
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public UnitOfMeasure getUom() {
-        return uom;
-    }
-
-    public void setUom(UnitOfMeasure uom) {
-        this.uom = uom;
-    }
 }
