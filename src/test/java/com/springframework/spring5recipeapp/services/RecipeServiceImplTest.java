9643877adcc3db19c4ddb8486738cbcfc448c67d
Anna Łuczak <an.archeo@gmail.com>
2022-02-10 23:36:44 +0000

    created 1 mockito test example for RecipeserviceImpl

---
+++ src/test/java/com/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.springframework.spring5recipeapp.services;
+
+import com.springframework.spring5recipeapp.domain.Recipe;
+import com.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() throws Exception{
+        Recipe recipe = new Recipe();
+        HashSet recipeData = new HashSet();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();// if it runs exactly 1 time
+    }
+}
