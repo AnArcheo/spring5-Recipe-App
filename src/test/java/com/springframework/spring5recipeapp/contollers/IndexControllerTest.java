3ade7c729dae502e84e3b19582f0d00629718554
Anna Łuczak <an.archeo@gmail.com>
2022-02-11 21:58:14 +0000

    fixed mockito test for Indexcontroller

--- src/test/java/com/springframework/spring5recipeapp/contollers/IndexControllerTest.java
+++ src/test/java/com/springframework/spring5recipeapp/contollers/IndexControllerTest.java
@@ -1,12 +1,18 @@
 package com.springframework.spring5recipeapp.contollers;
 
+import com.springframework.spring5recipeapp.domain.Recipe;
 import com.springframework.spring5recipeapp.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,9 +33,21 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
+        // for the Set testing
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(1L); //when using2X: recipes.add(new Recipe()); then both recipes has null id's giving only one recipe
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
         String viewName = indexController.getIndexPage(model);
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
