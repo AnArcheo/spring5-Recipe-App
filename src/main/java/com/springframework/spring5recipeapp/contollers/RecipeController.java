204757cb34dad407d6c8c4ef89f44cdb9675f4d0
Anna Łuczak <an.archeo@gmail.com>
2022-02-16 21:49:09 +0000

    updated RecipeController

--- src/main/java/com/springframework/spring5recipeapp/contollers/RecipeController.java
+++ src/main/java/com/springframework/spring5recipeapp/contollers/RecipeController.java
@@ -19,7 +19,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @GetMapping("/recipe/{id}/show")
+    @GetMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model){
 
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
