d43c2009b749acf24ff834f94746ed96ed38027d
Anna Łuczak <an.archeo@gmail.com>
2022-02-18 23:31:36 +0000

    Updated Recipe service and recipe service implementation classes

--- src/main/java/com/springframework/spring5recipeapp/services/RecipeService.java
+++ src/main/java/com/springframework/spring5recipeapp/services/RecipeService.java
@@ -1,5 +1,6 @@
 package com.springframework.spring5recipeapp.services;
 
+import com.springframework.spring5recipeapp.commands.RecipeCommand;
 import com.springframework.spring5recipeapp.domain.Recipe;
 
 import java.util.Set;
@@ -8,5 +9,11 @@ public interface RecipeService {
 
     Set<Recipe> getRecipes();
 
-    Recipe findById(Long id);
+    Recipe findById(Long l);
+
+    RecipeCommand findCommandById(Long l);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
+
+    void deleteById(Long idToDelete);
 }
