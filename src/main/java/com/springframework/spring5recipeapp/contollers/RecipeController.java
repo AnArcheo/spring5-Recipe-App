f3fbe8ebce21dc828ac6016a185339f6d40251cd
Anna Łuczak <an.archeo@gmail.com>
2022-02-19 20:52:43 +0000

    Added ability to add new recipe. Closes #5

--- src/main/java/com/springframework/spring5recipeapp/contollers/RecipeController.java
+++ src/main/java/com/springframework/spring5recipeapp/contollers/RecipeController.java
@@ -1,12 +1,14 @@
 package com.springframework.spring5recipeapp.contollers;
 
+import com.springframework.spring5recipeapp.commands.RecipeCommand;
 import com.springframework.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -27,12 +29,12 @@ public class RecipeController {
         return "recipe/show";
     }
 
-//    @GetMapping("recipe/new")
-//    public String newRecipe(Model model){
-//        model.addAttribute("recipe", new RecipeCommand());
-//
-//        return "recipe/recipeform";
-//    }
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
 
 //    @GetMapping("recipe/{id}/update")
 //    public String updateRecipe(@PathVariable String id, Model model){
@@ -40,44 +42,12 @@ public class RecipeController {
 //        return RECIPE_RECIPEFORM_URL;
 //    }
 
-//    @PostMapping("recipe")
-//    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
-//
-//        if(bindingResult.hasErrors()){
-//
-//            bindingResult.getAllErrors().forEach(objectError -> {
-//                log.debug(objectError.toString());
-//            });
-//
-//            return RECIPE_RECIPEFORM_URL;
-//        }
-//
-//        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
-//
-//        return "redirect:/recipe/" + savedCommand.getId() + "/show";
-//    }
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
-//    @GetMapping("recipe/{id}/delete")
-//    public String deleteById(@PathVariable String id){
-//
-//        log.debug("Deleting id: " + id);
-//
-//        recipeService.deleteById(Long.valueOf(id));
-//        return "redirect:/";
-//    }
+        return "redirect:/recipe/show/" + savedCommand.getId();
+    }
 
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler(NotFoundException.class)
-//    public ModelAndView handleNotFound(Exception exception){
-//
-//        log.error("Handling not found exception");
-//        log.error(exception.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//
-//        modelAndView.setViewName("404error");
-//        modelAndView.addObject("exception", exception);
-//
-//        return modelAndView;
-//    }
 }
