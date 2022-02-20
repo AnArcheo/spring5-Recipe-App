e0f5f79d2ffbf92ba9efc7145a156de2e0ac5803
Anna Łuczak <an.archeo@gmail.com>
2022-02-20 23:18:42 +0000

    Updated IngredientService and IngredientServiceImpl

--- src/main/java/com/springframework/spring5recipeapp/services/IngredientService.java
+++ src/main/java/com/springframework/spring5recipeapp/services/IngredientService.java
@@ -4,5 +4,9 @@ import com.springframework.spring5recipeapp.commands.IngredientCommand;
 
 public interface IngredientService {
 
-    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long id);
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
+
+    void deleteById(Long recipeId, Long idToDelete);
 }
